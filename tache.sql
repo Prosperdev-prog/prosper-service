407cdecc36a466d712498b3729ddfa384d5d8f7d
Prosperdev-prog <pzannou05@gmail.com>
2025-02-14 11:33:28 +0100

    Update tache.sql
    modification de la base de donnees

--- tache.sql
+++ tache.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le : mar. 23 jan. 2024 à 23:44
--- Version du serveur : 8.2.0
--- Version de PHP : 8.2.13
+-- Hôte : 127.0.0.1
+-- Généré le : ven. 14 fév. 2025 à 11:31
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `gestion-de-tache`
+-- Base de données : `ecole`
 --
 
 -- --------------------------------------------------------
@@ -27,24 +27,66 @@ SET time_zone = "+00:00";
 -- Structure de la table `tache`
 --
 
-DROP TABLE IF EXISTS `tache`;
-CREATE TABLE IF NOT EXISTS `tache` (
-  `id_tache` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tache` (
+  `id_tache` int(11) NOT NULL,
   `nom_tache` varchar(50) NOT NULL,
   `categorie_tache` varchar(30) NOT NULL,
   `description_tache` varchar(100) NOT NULL,
   `delai_tache` date NOT NULL,
-  PRIMARY KEY (`id_tache`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+  `date_echeance` date DEFAULT NULL,
+  `etat_de_tache` varchar(30) DEFAULT NULL,
+  `assignation` varchar(50) DEFAULT NULL,
+  `priorite` int(11) DEFAULT NULL,
+  `commentaire` text DEFAULT NULL,
+  `description` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Déchargement des données de la table `tache`
 --
 
-INSERT INTO `tache` (`id_tache`, `nom_tache`, `categorie_tache`, `description_tache`, `delai_tache`) VALUES
-(2, 'vjhùplkhjg', 'dgyjhpo$kyj', 'fghnm^fl:jlh', '2024-01-25'),
-(3, 'dfgh', 'dfghtr', 'dtgrt', '2024-01-23'),
-(4, 'DAMIEN', 'WILLIAM', 'Yann', '2024-01-24');
+INSERT INTO `tache` (`id_tache`, `nom_tache`, `categorie_tache`, `description_tache`, `delai_tache`, `date_echeance`, `etat_de_tache`, `assignation`, `priorite`, `commentaire`, `description`) VALUES
+(5, '', '', '', '0000-00-00', '2025-02-02', 'En cours', 'mtv', 1, '749/*7', 'dcvsfvb');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(200) NOT NULL,
+  `user_email` varchar(200) NOT NULL,
+  `statut` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `tache`
+--
+ALTER TABLE `tache`
+  ADD PRIMARY KEY (`id_tache`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `tache`
+--
+ALTER TABLE `tache`
+  MODIFY `id_tache` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
